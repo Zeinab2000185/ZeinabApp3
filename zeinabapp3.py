498ca1e8cf407d0914e251efc26366ea52b687da
Zeinab2000185 <134695158+Zeinab2000185@users.noreply.github.com>
2023-09-17 21:00:43 +0300

    Update zeinabapp3.py

--- zeinabapp3.py
+++ zeinabapp3.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-from streamlit.components.v1 import components
 
 # Read the data
 df = pd.read_csv("cause_of_deaths.csv")
@@ -26,15 +25,12 @@ df['Total Deaths'] = df[Cause_of_death].sum(axis=1)
 # Create a Streamlit app
 st.title('Global Health Data Overview')
 
-# Create a sidebar with tabs
-selected_tab = st.sidebar.radio("Select Tab:", ["Overview", "Choropleth Map"])
+# Create tabs for each visualization
+st.sidebar.title("Select Visualization")
+selected_viz = st.sidebar.radio("", ["Total Deaths by Country", "Top 5 Diseases in Lebanon", "Total Deaths in Lebanon Over the Years"])
 
-# Overview Tab
-if selected_tab == "Overview":
-    # Description
-    st.write("This app shows different visuals related to the Causes of Deaths dataset from Kaggle, ranging from 1990 to 2019, worldwide.")
-    
-    # Scatterplot by Country
+# Total Deaths by Country Tab
+if selected_viz == "Total Deaths by Country":
     st.header('Total Deaths by Country from 1990 to 2019')
     fig = px.scatter(
         df,
@@ -46,6 +42,8 @@ if selected_tab == "Overview":
     )
     st.plotly_chart(fig)
 
+# Top 5 Diseases in Lebanon Tab
+elif selected_viz == "Top 5 Diseases in Lebanon":
     # Filtering data for Lebanon
     lebanon_data = df[df['Country/Territory'] == 'Lebanon']
 
@@ -55,8 +53,7 @@ if selected_tab == "Overview":
     # Sorting the diseases by total deaths in descending order and select the top 5
     top_5_diseases = lebanon_disease_deaths.sort_values(ascending=False).head(5)
 
-    # Pie chart for Top 5 Diseases in Lebanon
-    st.subheader('Top 5 Diseases in Lebanon by Total Deaths')
+    st.header('Top 5 Diseases in Lebanon by Total Deaths')
     fig_pie = px.pie(names=top_5_diseases.index, values=top_5_diseases)
 
     # Customizing the layout
@@ -64,25 +61,12 @@ if selected_tab == "Overview":
 
     st.plotly_chart(fig_pie)
 
-    # Scatterplot for Total Deaths in Lebanon Over the Years
-    st.subheader('Total Deaths in Lebanon Over the Years')
+# Total Deaths in Lebanon Over the Years Tab
+elif selected_viz == "Total Deaths in Lebanon Over the Years":
+    # Filtering data for Lebanon
+    lebanon_data = df[df['Country/Territory'] == 'Lebanon']
+
+    st.header('Total Deaths in Lebanon Over the Years')
     fig_lebanon = px.scatter(lebanon_data, x='Year', y='Total Deaths', title='Total Deaths in Lebanon Over the Years')
 
     st.plotly_chart(fig_lebanon)
-
-# Choropleth Map Tab
-elif selected_tab == "Choropleth Map":
-    st.header('Cardiovascular Disease Deaths (1990-2019) Choropleth Map')
-    fig_choropleth = px.choropleth(
-        df,
-        locations="Country/Territory",
-        locationmode="country names",
-        color="Cardiovascular Diseases",
-        animation_frame="Year",
-        color_continuous_scale="Viridis",
-        title="Cardiovascular Disease Deaths (1990-2019)",
-        hover_name="Country/Territory",
-        projection="natural earth"
-    )
-
-    st.plotly_chart(fig_choropleth)
